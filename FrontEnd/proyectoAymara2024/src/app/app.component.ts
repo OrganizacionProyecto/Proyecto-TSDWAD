229da8b0295703058f7cc1ffcef2316b8ec7cdf2
Cecilia Cogot <ceciliacogot@gmail.com>
2024-05-07 10:58:01 -0300

    se inicializa el proyecto

--- FrontEnd/proyectoAymara2024/src/app/app.component.ts
+++ FrontEnd/proyectoAymara2024/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyectoAymara2024';
+  title = "Productos Naturales Aymara";
 }
