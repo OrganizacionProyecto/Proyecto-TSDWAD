8f2da123d7c2b85218eb070572ac6cb820abad9b
Nico2384 <damiannicolasfigueroa2024@gmail.com>
2024-05-11 11:09:50 -0300

    Actualizacion

--- Proyecto-TSDWAD/AymaraAPP/models.py
+++ Proyecto-TSDWAD/AymaraAPP/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here  
 
-#Modelo de la tabla Categoria
+#Modelo de la tabla Categoria.
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False)
