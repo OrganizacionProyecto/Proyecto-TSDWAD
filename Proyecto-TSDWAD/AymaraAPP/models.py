233377ae7cd7dd0a6cf5166ddded421dce0cb394
Nico2384 <damiannicolasfigueroa@gmail.com>
2024-05-11 10:37:33 -0300

    Probando user

--- Proyecto-TSDWAD/AymaraAPP/models.py
+++ Proyecto-TSDWAD/AymaraAPP/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here
 
+#Modelo de la tabla Categoria
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False)
@@ -14,3 +15,5 @@ class Categoria(models.Model):
         return self.nombre
     def __str__(self):
         return self.nombre
+    
+#Modelo de la tabla Metodo de Pago    
