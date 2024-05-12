064e9e90b73f21ca9811ed0e3f59270df1fd46c2
Walter <106196739+waltercamino@users.noreply.github.com>
2024-05-12 08:42:48 -0300

    agregado el componente login con las rutas

--- FrontEnd/proyectoAymara2024/src/app/app.routes.ts
+++ FrontEnd/proyectoAymara2024/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "products", component: ProductsComponent },
     { path: "products/:id", component: ProductDetailComponent },
     { path: "contact", component: ContactComponent },
+    { path:"login", component: LoginComponent},
     { path: "**", redirectTo: "", pathMatch: 'full' }
 ];
