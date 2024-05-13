f31afa768aad260935bfb4f9e35a61513a111fd6
IAmPurpleBoy <purpleboy@hotmail.com>
2024-05-13 02:22:43 -0300

    Se actualiza con Pedido y Usuario

--- Proyecto-TSDWAD/AymaraAPP/admin.py
+++ Proyecto-TSDWAD/AymaraAPP/admin.py
@@ -4,6 +4,8 @@ from .models import Categoria
 from .models import Metodo_pago
 from .models import Producto
 from .models import Carrito
+from .models import Pedido
+from .models import Usuario
 
 # Register your models here.
 
@@ -13,14 +15,22 @@ class CategoriaAdmin(admin.ModelAdmin):
 class Metodo_pagoAdmin(admin.ModelAdmin):
     list_display = ("nombre",)
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id_producto', 'nombre', 'descripcion', 'precio', 'disponibilidad', 'imagen', 'id_categoria')
 
-admin.site.register(Metodo_pago, Metodo_pagoAdmin)
 class CarritoAdmin(admin.ModelAdmin):
     list_display = ("id_carrito", "cantidad", "precio_unitario", "id_pedido", "id_producto", "id_usuario")
 
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('productoID', 'nombre', 'descripcion', 'precio', 'disponibilidad', 'imagen', 'id_categoria')
-    
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ("id_pedido", "fecha_pedido", "estado", "id_usuario")
+    	
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("id_usuario", "nombre", "apellido", "correo_electronico", "contrasena", "direccion", "tipo_usuario")
+
+
 admin.site.register(Categoria, CategoriaAdmin)    
 admin.site.register(Carrito, CarritoAdmin)
-admin.site.register(Producto, ProductoAdmin) 
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Metodo_pago, Metodo_pagoAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
