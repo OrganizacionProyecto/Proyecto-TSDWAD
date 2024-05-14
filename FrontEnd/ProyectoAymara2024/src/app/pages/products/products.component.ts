8a4c2a6b679be8817492d708d1c1df7bfd71c1e2
xNachooox2442 <nachobonzon1@gmail.com>
2024-05-14 00:10:02 -0300

    change3

--- FrontEnd/ProyectoAymara2024/src/app/pages/products/products.component.ts
+++ FrontEnd/ProyectoAymara2024/src/app/pages/products/products.component.ts
@@ -1,35 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-import { ApiService } from '../../service/api.service';
-import { IProduct } from '../../models/product.models';
-import  { Router} from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent implements OnInit{
+export class ProductsComponent {
 
-  productList: IProduct[] = []
-  private _apiService = inject (ApiService);
-  private _router = inject (Router);
-
-
-  ngOnInit(): void {
-    this._apiService.getProducts().subscribe ((data: IProduct[]) => {
-      console.log(data)
-      this.productList = data;
-    }
-    );
-  }
-
-    navegate(id: number): void{(
-      this._router.navigate(['/products',id])
-    )}
-
-  }
-
-
+}
