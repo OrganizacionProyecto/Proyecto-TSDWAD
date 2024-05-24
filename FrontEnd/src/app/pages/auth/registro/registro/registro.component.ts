3948b1f10397f61f0ae29b38122aa5bfbebb9a29
Walter <106196739+waltercamino@users.noreply.github.com>
2024-05-24 20:03:06 -0300

    campos requeridos en formulario registro

--- FrontEnd/src/app/pages/auth/registro/registro/registro.component.ts
+++ FrontEnd/src/app/pages/auth/registro/registro/registro.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { UsuarioService } from './usuario.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
   registroForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private usuarioService: UsuarioService, private router: Router) {
+  constructor(private fb: FormBuilder) {
     this.registroForm = this.fb.group({
       Nombre: ['', Validators.required],
       Apellido: ['', Validators.required],
       Correo_electronico: ['', [Validators.required, Validators.email]],
-      Contrasena: ['', Validators.required],
+      Contrasena: ['', [Validators.required, Validators.minLength(6)]],
       Direccion: ['', Validators.required],
       TipoUsuario: ['', Validators.required]
     });
@@ -28,15 +25,8 @@ export class RegistroComponent {
 
   onSubmit() {
     if (this.registroForm.valid) {
-      this.usuarioService.registrarUsuario(this.registroForm.value).subscribe(
-        response => {
-          console.log('Usuario registrado con éxito', response);
-          this.router.navigate(['/login']); // Redirige al usuario a la página de login después del registro
-        },
-        error => {
-          console.error('Error al registrar el usuario', error);
-        }
-      );
+      // Lógica de envío del formulario
+      console.log(this.registroForm.value);
     }
   }
 }
