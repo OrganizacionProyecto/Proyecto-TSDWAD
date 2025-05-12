e86a00dfc3f085312f50d9a38b0425e7cec6c6e6
JoaquinCamino12 <joaquincamino09@gmail.com>
2025-05-11 21:53:11 -0300

    .

--- FrontEnd/src/app/core/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id?: number;
-    username: string;
-    email: string;
-    app_role: 'admin_app' | 'editor' | 'viewer';
-    access?: string;
-    refresh?: string;
-  }
