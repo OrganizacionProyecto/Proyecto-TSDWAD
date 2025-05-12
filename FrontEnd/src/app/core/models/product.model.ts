873e2ca4ca5b8b9cd2f5d86c6a4927a370d73593
Conichan <conichan139@gmail.com>
2025-05-12 17:56:40 -0300

    Auto-generated commit

--- FrontEnd/src/app/core/models/product.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id_producto?: number;
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    stock: number;
-    imagen?: string | File;
-    id_categoria: number;
-  }
